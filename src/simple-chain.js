bb663ede330802d041bd89981ed74e9cccbe2185
inessa <inessasmorgunova@gmail.com>
2023-04-16 17:04:19 +0300

    add:fifteen ticket

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,32 +1,38 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    chain: [],
+    getLength() {
+        return this.chain.length;
+    },
+    addLink(value) {
+        throw new NotImplementedError('Not implemented');
+        if (arguments.length === 0) {
+            this.chain.push('( )');
+        } else {
+            this.chain.push(`( ${value} )`); // добавляем новый элемент в цепочку
+        }
+        return this; // возвращаем объект для возможности цепочного вызова
+    },
+
+    removeLink(/* position */) {
+        throw new NotImplementedError('Not implemented');
+        // remove line with error and write your code here
+    },
+    reverseChain() {
+        throw new NotImplementedError('Not implemented');
+        // remove line with error and write your code here
+    },
+    finishChain() {
+        throw new NotImplementedError('Not implemented');
+        // remove line with error and write your code here
+    }
 };
 
 module.exports = {
-  chainMaker
+    chainMaker
 };
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,11 +11,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  let t = arr.filter(a=>a!==(-1))
+  let s = t.sort((a,b)=> a-b)
+  return  arr.map(a=>a ===(-1) ? -1: s.shift())
+
 }
 
 module.exports = {
   sortByHeight
 };
+console.log(sortByHeight([ 11, 16, 2, 2, 4, 9 ] ))

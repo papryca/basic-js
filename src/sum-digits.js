e91ec708fde0db251776eb39c75abc6c92180205
inessa <inessasmorgunova@gmail.com>
2023-04-16 17:25:16 +0300

    add:sixteen ticket

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 /**
  * Given a number, replace this number with
@@ -12,11 +12,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+    let s = n.toString();
+
+    while (s.length > 1) {
+        s = s.split('')
+            .map(n => Number(n))
+            .reduce((partialSum, a) => partialSum + a, 0)
+            .toString()
+    }
+
+    return Number(s);
 }
 
 module.exports = {
-  getSumOfDigits
+    getSumOfDigits
 };
+console.log(getSumOfDigits(91))

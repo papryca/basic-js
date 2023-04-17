708e440dab06b639b6ed249e7b8e5e6ea20f6958
inessa <inessasmorgunova@gmail.com>
2023-04-17 19:00:00 +0300

    fix:twenty ticket

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -6,31 +6,40 @@ const {NotImplementedError} = require('../extensions/index.js');
  */
 const chainMaker = {
     chain: [],
+
     getLength() {
         return this.chain.length;
     },
+
     addLink(value) {
-        throw new NotImplementedError('Not implemented');
-        if (arguments.length === 0) {
-            this.chain.push('( )');
-        } else {
-            this.chain.push(`( ${value} )`); // добавляем новый элемент в цепочку
-        }
-        return this; // возвращаем объект для возможности цепочного вызова
+        this.chain.push(`( ${value} )`);
+        return this;
     },
 
-    removeLink(/* position */) {
-        throw new NotImplementedError('Not implemented');
-        // remove line with error and write your code here
+    removeLink(position) {
+        if (
+            position <= 0 ||
+            position > this.chain.length ||
+            position % 1 !== 0
+        ) {
+            this.chain = [];
+            throw new Error("You can't remove incorrect link!");
+        }
+        this.chain.splice(position - 1, 1);
+        return this;
     },
+
     reverseChain() {
-        throw new NotImplementedError('Not implemented');
-        // remove line with error and write your code here
+        this.chain.reverse();
+        return this;
     },
+
     finishChain() {
-        throw new NotImplementedError('Not implemented');
-        // remove line with error and write your code here
-    }
+        const result = this.chain.join("~~");
+        this.chain = [];
+        return result;
+    },
+
 };
 
 module.exports = {
